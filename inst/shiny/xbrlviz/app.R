94930af87b55641072fe61e8f409d7f2110f1507
Brad West <brad.west@workiva.com>
2017-06-21 17:04:16 -0600

    Zoomable app with datatables and limited interactivity

--- inst/shiny/xbrlviz/app.R
+++ inst/shiny/xbrlviz/app.R
@@ -1,9 +1,12 @@
 library(shiny)
 library(xbrlr)
 library(scales)
+library(DT)
+
 
 source("./side_bar.R")
 source("./pretty_tree_graph.R")
+source("./layout_rescale.R")
 
 # calculation linkbase
 # in the future will need to write if else statement for loading data depending
@@ -30,39 +33,93 @@ ui <- fluidPage(
           create_sidebar(calc_link_dirty, input$link),
           radioButtons(inputId = "names", label = "Show Element Names?",
                        choices = c("Yes" = TRUE, "No" = FALSE),
-                       selected = FALSE),
-          verbatimTextOutput("info")
+                       selected = FALSE)
           ),
 
       mainPanel(
           # height fills 15in retina screen
-          plotOutput("tree", width="1300px", height = "1200px",
+          plotOutput("tree", width="1300px", height = "800px",
                      click = "plot_click",
-                     brush = "plot_brush")
+                     brush = brushOpts(id = "plot_brush", resetOnNew = TRUE),
+                     dblclick = "plot_dblclick"
+                     ),
+          h3("Clicked Element"),
+          DT::dataTableOutput("plot_clicked_points"),
+          h3("Selected Elements"),
+          DT::dataTableOutput("plot_brushed_points")
       )
    )
 )
 
 
 server <- function(input, output) {
+
+    ranges <- reactiveValues(x = c(-1,1), y = c(-1,1))
+
+    # output$tree <- renderSvgPanZoom({
+    #     # plot.window(xlim = if (!is.null(ranges$x)) ranges$x else c(-1,1),
+    #     # ylim =  if (!is.null(ranges$y)) ranges$y else c(-1,1))
+    #     zoom_level <- 2 / (ranges$x[2] - ranges$x[1])
+    #     g <- pretty_tree_graph(input$statement, calc_link_dirty, input$link,
+    #                       names = input$names,
+    #                       xlim = ranges$x,
+    #                       ylim = ranges$y,
+    #                       zoom = zoom_level)
+    #     svgPanZoom(g, controlIconsEnabled = TRUE)
+    # })
+
     output$tree <- renderPlot({
+        # plot.window(xlim = if (!is.null(ranges$x)) ranges$x else c(-1,1),
+                    # ylim =  if (!is.null(ranges$y)) ranges$y else c(-1,1))
+        zoom_level <- 2 / (ranges$x[2] - ranges$x[1])
         pretty_tree_graph(input$statement, calc_link_dirty, input$link,
-                          names = input$names)
+                          names = input$names,
+                          xlim = ranges$x,
+                          ylim = ranges$y,
+                          zoom = zoom_level)
     })
 
     # get the layout of the graph
-    output$info <- renderPrint({
-        stmt_of_interest <- find_statement(
-            input$statement, calc_link_dirty, input$link)
-        graph <- create_graph(stmt_of_interest)
-        l = igraph::layout_as_tree(graph)
-        # need to rescale to -1 to 1 plotting region
-        x <- rescale(l[,1], to = c(-1,1))
-        y <- rescale(l[,2], to = c(-1,1))
-        # bind the coordinates to the statement
-        coord_df <- cbind(x,y,stmt_of_interest)
-        nearPoints(coord_df, input$plot_click, xvar = "x", yvar = "y")
-        brushedPoints(coord_df, input$plot_brush, xvar = "x", yvar = "y")
+    # output$info <- renderPrint({
+    #     coord_df <- rescale_layout(input$statement, calc_link_dirty,
+    #                                input$link)
+    #     nearPoints(coord_df, input$plot_click, xvar = "x", yvar = "y")
+    # })
+    output$plot_clicked_points <- DT::renderDataTable({
+        coord_df <- rescale_layout(input$statement, calc_link_dirty,
+                                   input$link)
+        res <- nearPoints(
+            coord_df, input$plot_click, xvar = "x", yvar = "y")[, c(4:7)]
+        datatable(res,
+                  colnames =
+                      c("Element Label",
+                        "Depth From Root",
+                        "Add/Subtract",
+                        "Parent Element Name"))
+    })
+
+    output$plot_brushed_points <- DT::renderDataTable({
+        coord_df <- rescale_layout(input$statement, calc_link_dirty,
+                                   input$link)
+        res <- brushedPoints(
+            coord_df, input$plot_brush, xvar = "x", yvar = "y")[, c(4:7)]
+        datatable(res, colnames =
+                      c("Element Label",
+                        "Depth From Root",
+                        "Add/Subtract",
+                        "Parent Element Name"))
+    })
+
+    observeEvent(input$plot_dblclick, {
+        brush <- input$plot_brush
+        if (!is.null(brush)) {
+            ranges$x <- c(brush$xmin, brush$xmax)
+            ranges$y <- c(brush$ymin, brush$ymax)
+
+        } else {
+            ranges$x <- c(-1,1)
+            ranges$y <- c(-1,1)
+        }
     })
 }
 
